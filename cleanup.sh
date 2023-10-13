ffd173133a3269b3cd90ea6b2a1a31236616e9b5
Arijit Ghosh <92109154+Rancho2002@users.noreply.github.com>
2023-10-14 01:26:37 +0530

    Create cleanup.sh

---
+++ cleanup.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+sudo apt-get update #Updates the package lists for upgrades and new installations
+sudo apt-get upgrade -y	#upgrade all softwares to latest version
+sudo apt-get autoremove && sudo apt-get autoclean #autoremove removes packages that were automatically installed but are no longer required and autoclean removes unnecessary packages and files from the package cache
+sudo apt-get clean #Deletes all packages from the package cache.
+sudo journalctl --rotate #Rotates and archives the journal files
+sudo journalctl --vacuum-time 1d #Clears all log files older than 1 day.
+rm -rf ~/.cache/thumbnails/* #Removes thumbnail cache files
+# Removes old revisions of snaps
+# CLOSE ALL SNAPS BEFORE RUNNING THIS
+set -eu
+LANG=C snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        snap remove "$snapname" --revision="$revision"
+    done
+notify-send "Cleaning done"
